ef5a1255b19a2a3c5d9fd3781eb0d11bf1923a0e
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2017-09-12 16:54:50 +0530

    Rename positive.c to aa.c

---
+++ aa.c
@@ -0,0 +1,12 @@
++#include<stdio.h>
+ +void main()
+ +{
+ +int n;
+ +scanf("%d",&n);
+ +if(n>0)
+ +printf("%d the number is positive");
+ +ifelse(n<0)
+ +printf("%d the number is negative");
+ +else
+ +printf("%d the number is zero");
+ +}
