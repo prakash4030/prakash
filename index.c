d748326173586113ab4690e6088336f07655a447
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-21 14:01:40 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a[10],i,j,n=5;
+ for(i=0;i<n;i++)
+   {
+     scanf("%d",&a[i]); 
+     printf("%d\t%d\n",a[i],i);
+     getch();
+   }
