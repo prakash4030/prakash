ae67a87c8289a1ab7108c046409d7a76e16d71c3
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-21 14:55:22 +0530

    Create digitt.c

---
+++ digitt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    int count = 0;
+
+    printf("Enter a number:\t ");
+    scanf("%d", &n);
+
+    while(n != 0)
+    {
+        n /= 10;
+        ++count;
+    }
+
+    printf("Number of digits= %d", count);
+}
