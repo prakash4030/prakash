9fa481638677c0a74e8eee0590bfafdf2145c53c
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-21 13:52:20 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int arr[MAX_SIZE];
+    int i, N;
+    printf("Enter size of array: ");
+    scanf("%d", &N);
+    for(i=0; i<N; i++)
+    {
+    scanf("%d", &arr[i]);
+    }
+    printf("\nElements in array are");
+    for(i=0; i<N; i++)
+    {
+    printf("%d, ", arr[i]);
+    getch();
+    }
