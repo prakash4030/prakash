1946e52eaa95d4c8088f0a8781bbc83f903f5a42
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-21 13:59:26 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x;
+char ch;
+scanf("%d",&x);
+if((ch>=65&&ch<=90)||(ch>=97&&ch<=122))
+{
+printf("no");
+}
+else
+{
+printf("yes");
+}
+return 0;
+getch();
+}
