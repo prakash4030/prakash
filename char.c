98fef7f08157e3bb0b3b7a69fbd99c00159bca01
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-21 15:39:17 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void  main()
+{
+char string[60];
+int i,count=0;
+scanf("%s",&string[i]);
+while(string[i]!='\0')
+{
+if((string[i]>='a')&&(string[i]<='z'))
+{
+count+=1;
+}
+i++;
+}
+}
