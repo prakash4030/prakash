50afa5af933450fafe1332285da61edc3f82614c
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-21 14:11:53 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void  main()
+{
+int rem,sum=0,rks,num;
+scanf("%d",&num);
+rks=num;
+while(num>0)
+{
+rem=num%10;
+sum=sum*10+rem;
+num=num/10;
+}
+  if(sum==rks)
+    printf("palindrome");
+  else
+    printf("not palindrome");
+}
