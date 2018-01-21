b55bfe6dce0b615b14fbb3cd0624d07a74b9ee0e
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-21 14:06:10 +0530

    Create subrac.c

---
+++ subrac.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e,g;
+printf("enter the first timein hours and minutes");
+scanf("%d%d",&a,&c);
+printf("enter the second time in hours and minutes");
+scanf("%d%d",&b,&d);
+e=a-b;
+f=c-d;
+printf("the difference is %d%d",&e,&g);
+getch();
+}
