03e625092324b620d38235416a7147c0fdd0d1d5
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-23 10:51:33 +0000

    Rename fib.c to fibo.c

---
+++ fibo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int sum=0,num1,num2,j,n;
+scanf("%d%d%d",&num1,&num2,&n);
+for(j=0;j<n;j++)
+{
+sum=num1+num2;
+printf("%d\n",sum);
+num1=num2;
+num2=sum;
+}
+return 0;
+}
