e81aacb09bb2e594782ebda445bb0e899c1ee130
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-25 10:46:33 +0000

    Create strcattt.c

---
+++ strcattt.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[20],b[20];
+printf("enter two strings");
+gets(a);
+gets(b);
+strcat(a,b);
+printf("the  concatenated string is %s",a);
+}
+return0;
+getch();
+}
