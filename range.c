d3535e2f244c6fe2a6a61fdaaa5ad5429e2fec41
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-25 11:05:53 +0000

    Create range.c

---
+++ range.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    if(a>=1&&a<=10)
+    {
+        printf("number is within range");
+    }
+    else
+    {
+        printf("out side range");
+    }    
+}
