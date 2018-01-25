6fa186fedb27f9f3e9ca690d2e35224f1ff15744
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-01-25 11:14:09 +0000

    Create nextnum.c

---
+++ nextnum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,s;
+    printf("\nenter the a value :%d",a);
+    scanf("%d",&a);
+    s=a++;
+    printf("\nnext value is: %d",a);
+    return 0;
+    
+}
