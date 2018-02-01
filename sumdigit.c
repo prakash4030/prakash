0a83ff4ae07adda4cc5eb5381e8e408880c3d7c1
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-02-01 14:59:06 +0530

    Create sumdigit.c

---
+++ sumdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int n,i,a,sum=0;
+	printf("enter the number:");
+	scanf("%d",&n);
+
+while(n>0)
+{
+            a=n%10;
+            n=n/10;
+            sum=sum+a;
+}
+	
+	printf("the sum is %d",sum);
+  return 0;
+}
