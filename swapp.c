380d17da502d9cc9672fd6b565c035d1912e6681
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-02-08 14:23:48 +0530

    Rename swap.c to swapp.c

---
+++ swapp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+      double firstNumber, secondNumber, temporaryVariable;
+      clrscr();
+
+      printf("Enter first number: ");
+      scanf("%lf", &firstNumber);
+
+      printf("Enter second number: ");
+      scanf("%lf",&secondNumber);
+
+      
+      temporaryVariable = firstNumber;
+
+    
+      firstNumber = secondNumber;
+
+      
+      secondNumber = temporaryVariable;
+
+      printf("\nAfter swapping, firstNumber = %.2lf\n", firstNumber);
+      printf("After swapping, secondNumber = %.2lf", secondNumber);
+
+}
