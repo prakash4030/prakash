2f913fe48eff965d705440375b4cd9dc46dd4ab3
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-02-13 14:09:04 +0530

    Create rectarea.c

---
+++ rectarea.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float a,b,area;
+printf("the given number/n");
+scanf("%f",&a);
+printf("the given number/n");
+scanf("%f",&b);
+area=a*b;
+printf("%f",area);
+return 0;
+}
