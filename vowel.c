613fb347c28629c0df98631af1e848fcfb66a4fa
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-02-27 16:07:05 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i;
+char a[50];
+printf("/n Enter the word");
+scanf("%s",&a[i]);
+if((a[i]>=a)||(a[i]>=e)||(a[i]>=i)||(a[i]>=o)||(a[i]>=u))
+{
+printf("YES");
+}
+else
+{
+printf("NO);
+}
+return 0;
+}
