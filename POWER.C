296a536ce2e7892d501c7882bb2c4b67dc2ec469
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-02-27 15:46:45 +0530

    Create POWER.C

---
+++ POWER.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,power;
+printf("/n ENTER THE GIVEN WORD");
+scanf("%d%d",&a,&b);
+power=pow(a,b);
+printf("/n POWER OF GIVEN WORD IS %d",power);
+return 0;
+}
