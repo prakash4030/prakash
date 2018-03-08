88ad97add2a8b6313fea3e1cd348fa094f6bc7f9
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-03-08 16:22:12 +0530

    Create isogram.c

---
+++ isogram.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	char a[20];
+	int i,c,j,count=0;
+	scanf("%s",a);
+	c=strlen(a);
+	for(i=0;i<c;i++)
+	{
+		for(j=i+1;j<=c;j++)
+		{
+	if(a[i]==a[j])
+	{
+	count++;
+	}
+  }
+	}
+	if(count==0)
+	{
+	printf("yes it is isogram");
+	}
+	else
+	{
+	printf("not a isogram");
+	}
+  getch();
+	}
