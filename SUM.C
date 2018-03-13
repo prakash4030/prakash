fba657007497623ad7f15e5f40ab9e3718971be9
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-03-13 16:04:55 +0530

    Create SUM.C

---
+++ SUM.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	 int no[4],i,sum=0;
+	 for(i=0;i<4;i++)
+	 {
+	 scanf("%d",&no[i]);
+	 }
+	for(i=0;i<4;i++)
+	 {
+	 sum=sum+no[i];
+	 }
+	 printf("sum %d",sum);
+}
