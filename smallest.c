15805183fcfe9c328c9e2365a72bf1cd524c046e
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-03-13 16:36:41 +0530

    Create smallest.c

---
+++ smallest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n[5],a,i;
+	for(i=1;i<=5;i++)
+	{
+		scanf("%d",&n[i]);
+	}
+	printf("\nenter the num");
+	scanf("%d",&a);
+            for(i=1;i<=5;i++)
+            {
+            printf("\n%d",n[a]);
+            break;
+            }
+            getch();
+}
