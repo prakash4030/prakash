bd27ccf88e9ab776b956bd0b71b89c17b97e7f4e
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-03-15 16:36:57 +0530

    Create 113.c

---
+++ 113.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[50],b,c,i,count=0;
+scanf("%d%d",&b,&c);
+for(i=0;i<b;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<b;i++)
+{
+if(a[i]==c)
+{
+count++;
+}
+}
+printf("%d time",count);
+getch();
+}
+
