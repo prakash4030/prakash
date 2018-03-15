155a393859c436b95cb406d8cabd5202a927173e
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-03-15 16:12:19 +0530

    Create 78.C

---
+++ 78.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main
+{
+	int a;
+	scanf("%d",&a);
+	if(a%13==0)
+	printf("this is a multiple of 13");
+	else
+	printf("this is not a multiple of 13");
+  getch();
+}
