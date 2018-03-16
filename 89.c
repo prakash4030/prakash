f86ecf68466836bdfb197333f71b3b45171502cc
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-03-16 16:00:14 +0530

    Create 89.c

---
+++ 89.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+void main
+{
+char ch[100];
+char temp;
+gets(ch);
+int i,k,j;
+k=strlen(ch);
+for(i=0;i<k;i++)
+{
+for(j=i+1;j<k;j++)
+{
+	
+            if(ch[i]>ch[j])
+            {
+                temp=ch[i];
+                ch[i]=ch[j];
+                ch[j]=temp;
+}
+
+}
+}
+printf("%s",ch);
+}
