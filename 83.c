296486e71dd365975b2cd093490fc8e734116c41
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-03-16 15:51:26 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{	
+   int n1,n2;
+   char c;
+
+       scanf("%d %c %d",&n1,&c,&n2);
+       if(c=='/')
+       {
+           printf("%d\n",(n1/n2));
+       }
+       else
+       {
+           printf("%d\n",(n1%n2));
+       }
+       getch();
+}
