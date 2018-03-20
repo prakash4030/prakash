c106846714ca368ade9a7a540b6e18402b7712d5
prakash4030 <31503907+prakash4030@users.noreply.github.com>
2018-03-20 15:50:10 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int str;
+int a;
+printf("enter the string");
+scanf("%d",&str);
+if((str==0)||(str==1))
+{
+printf("binary representation");
+}
+else
+{
+printf("not a binary representation");
+return 0;
+}
